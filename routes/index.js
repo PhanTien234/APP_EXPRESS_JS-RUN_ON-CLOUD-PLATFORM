cdf4d3b0d8bff9c0d9ca6fda1d8096db4dc6b3a4
PhanTien234 <minhtienp328@gmail.com>
2022-10-13 09:19:27 +0700

    make form login base

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ATN SHOP' });
+});
+// Process for Post request here
+router.post('/', function(req, res, next) {
+  res.render('login', { title: 'ATN SHOP' });
 });
 
+router.post('/login', function(req, res, next) {
+  res.render('users', { title: 'Welcome to user' });
+});
 module.exports = router;
