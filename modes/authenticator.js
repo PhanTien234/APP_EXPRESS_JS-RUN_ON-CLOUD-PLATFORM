8e9e73e4334c20585fb467729de992b2cdf2c8e5
PhanTien234 <minhtienp328@gmail.com>
2022-10-18 11:05:41 +0700

    create table to show product by shopID

--- modes/authenticator.js
+++ modes/authenticator.js
@@ -1,16 +1,18 @@
 var pg_con = require('./pg_config')
 async function authen(user, pass){
-    var authenticated = false;
+    let authenticated = false;
+    let shop_id;
     const auth_query ={
         text: 'SELECT * FROM users WHERE name = $1 AND password = $2',
         values: [user, pass]
     };
     var query_data = await pg_con.query(auth_query);
-    if(query_data.rows.length ==1){
+    if(query_data.rowCount ==1){
         authenticated = true;
+        shop_id = query_data.rows[0].shop_id;
     }
     // console.log(authenticated);
-    return authenticated;
+    return [authenticated, shop_id];
 
 }
 module.exports = authen;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var authen = require('../modes/authenticator')
+var display_product = require('../modes/table_display')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,12 +17,15 @@ router.post('/login', async function(req, res, next) {
   let username =req.body.username;
   let password =req.body.password;
   console.log(username + ":" + password)
-  let authenticated = await authen(username, password)
+  let [authenticated, shop_id] = await authen(username, password)
   if(authenticated == true){
-    res.render('users', {title: 'welcome to user'})
+    let table = await display_product(shop_id);
+    res.render('users', {title: 'welcome to user', name: username, table_string: table})
   }else{
     res.render('login', { title: 'ATN SHOP', message: 'wrong user password' });
   }
 
 });
 module.exports = router;
+
+
